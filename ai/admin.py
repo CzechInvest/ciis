a001c3eef891c169ff7aec312da2723321938fb2
Jachym Cepicky <jachym.cepicky@gmail.com>
2019-10-29 15:06:50 +0100

    ai admin

--- ai/admin.py
+++ ai/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin, LeafletGeoAdminMixin
 from .models import *
 
+class AiAdmin(LeafletGeoAdmin):
+
+    raw_id_fields = ("address",)
+    default_zoom = 7
+    default_lon = 1730000
+    default_lat = 6430000
 
 admin.site.register(Form)
-admin.site.register(Ai)
+admin.site.register(Ai, AiAdmin)
