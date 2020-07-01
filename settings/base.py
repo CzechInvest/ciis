6e208acb7d0757c6d11233888bff5ed0530ab407
Jachym Cepicky <jachym.cepicky@gmail.com>
2020-07-01 13:22:01 +0000

    conf update

--- settings/base.py
+++ settings/base.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "cigeo/static")
+    #os.path.join(BASE_DIR, "cigeo/static")
 ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
