e2af7b778d2173da599da36c7b442b3b792c412c
Jachym Cepicky <jachym.cepicky@gmail.com>
2020-12-25 19:24:05 +0100

    debug wsgi

--- ciis/wsgi.py
+++ ciis/wsgi.py
@@ -15,6 +15,7 @@ from django.core.wsgi import get_wsgi_application
 # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
 # on Azure App Service and should use the production settings in production.py.
 
+print("################--", os.environ)
 settings_module = "settings.production" if 'WEBSITE_HOSTNAME' in os.environ else 'settings.local'
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_local")
