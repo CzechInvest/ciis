8388742511008516c85d6edc7c247350e5cc32a9
Jachym Cepicky <jachym.cepicky@gmail.com>
2020-12-29 20:41:24 +0100

    kv testing

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 service ssh start
 
-#python3 -m http.server 9000 
-gunicorn -b 0.0.0.0:9000 --error-logfile /var/ciis/logs/error.log ciis.wsgi
+python3 -m http.server 9000 
+#gunicorn -b 0.0.0.0:9000 --error-logfile /var/ciis/logs/error.log ciis.wsgi
 
