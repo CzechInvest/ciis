aade60205ab8ec07d59ba30785a7a2f7bc38e65b
Jachym Cepicky <jachym.cepicky@gmail.com>
2021-01-01 20:58:01 +0100

    updating init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 service ssh start
 
 #python3 -m http.server 9000 
-python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py migrate --settings settings.production
+python3 manage.py collectstatic --settings settings.production
 gunicorn -b 0.0.0.0:9000 --error-logfile /var/ciis/logs/error.log ciis.wsgi
 
