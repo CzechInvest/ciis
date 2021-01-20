59062097056d85c3951adb20813bd89cfaff1635
Jachym Cepicky <jachym.cepicky@gmail.com>
2021-01-20 15:34:27 +0100

    more logs

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ service ssh start
 #python3 -m http.server 9000 
 python3 manage.py migrate --settings settings.production
 python3 manage.py collectstatic --settings settings.production
-gunicorn -b 0.0.0.0:9000 --error-logfile /var/ciis/logs/error.log ciis.wsgi
+gunicorn -b 0.0.0.0:9000 --log-level debug --capture-output --log-file /var/ciis/logs/gunicorn.log --error-logfile /var/ciis/logs/error.log ciis.wsgi
 
