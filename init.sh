3bf095bc356d11cd3de0437dd8e087410aaf44d7
Jachym Cepicky <jachym.cepicky@gmail.com>
2021-01-29 19:01:27 +0100

    crontabfix

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ service ssh start
 #python3 -m http.server 9000 
 
 crontab -l > jobs.txt
-echo "0 0 7 * * python3 manage.py monthly_report `date -d \"$(date +%Y-%m-01) -1 day\" +'%Y-%m-%d'`" >> jobs.txt
-cron jobs.txt
+echo "0 0 7 * * python3 manage.py monthly_report date -d \"$(date +%Y-%m-01) -1 day\" +'%Y-%m-%d'" >> jobs.txt
+crontab jobs.txt
 
 python3 manage.py migrate --settings settings.production
 python3 manage.py collectstatic --settings settings.production
